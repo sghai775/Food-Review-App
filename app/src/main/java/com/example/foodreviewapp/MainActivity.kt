4a99221e2125ce5ae4fa663d770095e0c3d4ff59
Surya Ghai <suryaghai@techforgoodinc.org>
2025-05-04 15:36:18 -0400

    initial firebase setup

--- app/src/main/java/com/example/foodreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/foodreviewapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.foodreviewapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // just for testing firebase connection, can delete later
+        var firebase : DatabaseReference = FirebaseDatabase.getInstance().reference
+        var list = firebase.child("Restaurants")
+        list.child("Burger King").setValue("test1")
+
+
     }
 }
